6b6768d60753e9b2f97eb12ca472b247539a5906
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2013-09-12 11:05:59 +0100

    Codeword works! Code is awful though, so some serious refactoring is required.

--- wordmaster.pl
+++ wordmaster.pl
@@ -4,11 +4,16 @@ use strict;
 use warnings;
 
 use Getopt::Long qw( :config posix_default bundling no_ignore_case );
-our( @words, $type, $raw );
+our( @words, $type, $raw, $data, $known, $whole, $ends, $starts );
 
-GetOptions ( "type|t=s" => \$type,
-             "raw|r=s"    => \$raw,
-           );
+GetOptions ( "type|t=s"        => \$type,
+             "raw|r=s"         => \$raw,
+             "data|d=s"        => \$data,
+             "known|k=s"       => \$known,
+             "whole|w"         => \$whole,
+             "starts-with|s=s" => \$starts,
+             "ends-in|e=s"     => \$ends,
+);
 
 $type = $type || 'search';
 
@@ -28,5 +33,91 @@ if( $type eq 'search' ) {
         foreach my $word ( @words ) {
             print $word, "\n" if $word =~ /$raw/;
         }
+    }
+}
+
+if( $type eq 'codeword' ) {
+    # Codeword subroutine.
+    $whole = 1;
+
+    my %unknown = ();
+
+    # Parse input
+    $known =~ s/[\W]//g; # Sanitise known letters. Get rid of commas if there are any, etc.
+
+    # Get positions of unknown letters and their numbers.
+    my $length = length( $data );
+
+    my $tmp = $data;
+
+    my @heads = ();
+    my $head;
+    my $tail;
+    my $pos = 0;
+    my $slot = 0;
+
+    while ( ( my $index = index( $tmp, ']' ) ) != -1 ) {  # Find the end of a number marking.
+        
+
+        $head = substr( $tmp, 0, $index + 1 ); # Last character should be ']'.
+        push( @heads, ($head =~ /^([a-zA-Z]+)/ ? $1 : '' ) );
+        $tail = substr( $tmp, $index + 1 );
+
+        my $numindex = index( $head, '[' ); # Find the beginning of the number marking.
+        my $num = substr( $head, $numindex + 1, -1 );
+
+        $pos += $numindex + 1;
+
+        $unknown{$slot} = $num;
+        $slot++;
+        $tmp = $tail;
+    }
+
+    my %counts = ();
+
+    foreach my $key ( sort { $a <=> $b }  keys %unknown ) {
+
+        my $value = $unknown{$key};
+        push( @{ $counts{$value} }, $key );
+        
+    }
+
+    # Build regular expression.
+
+    my $regex;
+    my $subtract = 0;
+    foreach my $position ( sort { $a <=> $b } keys %unknown ) {
+        my $subregex;
+        if( $position == 0 ) {
+            $subregex = '([^' . $known . '])';
+        }
+        else {
+            my @tmparr = @{ $counts{ $unknown{ $position } } };
+            my $countindex;
+            if( @tmparr ) {
+                ( $countindex ) = grep { $tmparr[$_] eq $position } 0 .. ( scalar( @tmparr ) - 1);
+            }
+            if( scalar( @tmparr ) > 1 && $countindex > 0 ) {
+                $subregex = '\\' . ( $tmparr[0] + 1 );
+                $subtract++;
+            }
+            else {
+                for( my $i = 1; $i <= $position - $subtract; $i++ ) {
+                    $subregex .= '\\' . $i . '|';
+                }
+                $subregex =~ s/\|$//;
+                $subregex = '(?!' . ( $position > 1 ?  '(' . $subregex . ')' : $subregex ) . ')([^' . $known . '])';
+            }
+        }
+        $regex .= $heads[$position] . $subregex;
+    }
+    $regex .= $tail;
+
+    if( $whole ) {
+        $regex = '^' . $regex . '$';
+    }
+
+    foreach my $word ( @words ) {
+        print $word, "\n" if $word =~ /$regex/;
     }
 }
